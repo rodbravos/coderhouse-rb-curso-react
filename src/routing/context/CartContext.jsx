6e44017af58cb703afa04d46bbb540fc9a1bc48e
Rodrigo Bravo Solis <rodbravo.s@gmail.com>
2024-02-29 08:39:59 -0300

    Create CartContext.jsx

---
+++ src/routing/context/CartContext.jsx
@@ -0,0 +1,70 @@
+import { createContext, useContext, useState } from "react";
+
+const cartContext = createContext();
+
+export const { Provider } = cartContext;
+
+export const useCartContext = () => useContext(cartContext);
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const [itemsTotal, setItemsTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const addItem = (item, quantity) => {
+    const existingItemIndex = cart.findIndex(
+      (cartItem) => cartItem.item.id === String(item.id)
+    );
+
+    if (existingItemIndex !== -1) {
+      // Item already exists, update the quantity
+      const updatedCart = [...cart];
+      updatedCart[existingItemIndex] = {
+        ...updatedCart[existingItemIndex],
+        quantity: updatedCart[existingItemIndex].quantity + quantity,
+      };
+
+      // Update the total based on the price of the item and its new quantity
+      setTotal(
+        (prevTotal) =>
+          prevTotal +
+          item.price *
+            (updatedCart[existingItemIndex].quantity -
+              updatedCart[existingItemIndex].quantity)
+      );
+
+      setCart(updatedCart);
+    } else {
+      // Item does not exist, create a new entry
+      setCart((prevCart) => [...prevCart, { item, quantity }]);
+
+      // Update the total based on the price of the item and its new quantity
+      setTotal((prevTotal) => prevTotal + item.price * quantity);
+    }
+
+    // Update the itemsTotal based on the new quantity
+    setItemsTotal((prevItemsTotal) => prevItemsTotal + quantity);
+  };
+
+  const removeItem = (id) => {};
+
+  const clear = () => {
+    setCart([]);
+    setItemsTotal(0);
+    setTotal(0);
+  };
+
+  // const isInCart = (id) => cart.find((item) => item.item.id === id);
+
+  const valorDelContexto = {
+    cart,
+    itemsTotal,
+    total,
+    addItem,
+    removeItem,
+    clear,
+  };
+  return <Provider value={valorDelContexto}>{children}</Provider>;
+};
+
+export default CartProvider;
